6386fa93ac63d21edc08cb3b30ecd1a4df288029
xuan son tran <xuanson33bk@gmail.com>
2016-08-08 13:51:12 +0700

    * missing UninstallInformation class

---
+++ RegUtils/UninstallInformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RegUtils
+{
+    public class UninstallInformation
+    {
+        public string DisplayName { get; set; }
+        public string DisplayIcon { get; set; }
+        public string DisplayVersion { get; set; }
+        public string HelpLink { get; set; }
+        public string Publisher { get; set; }
+        public string UninstallString { get; set; }
+        public string URLInfoAbout { get; set; }
+        public string Comments { get; set; }
+        public string Contact { get; set; }
+        public DateTime InstallDate { get; set; }
+        public string InstallLocation { get; set; }
+        public int EstimateSize { get; set; }
+    }
+}
