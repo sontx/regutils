d929e717cf489f5f01bd5648fbddaae930028841
xuan son tran <xuanson33bk@gmail.com>
2016-08-08 14:52:59 +0700

    * missing Architecture and ProgramScope enums

---
+++ RegUtils/Architecture.cs
@@ -0,0 +1,18 @@
+﻿namespace RegUtils
+{
+    public enum Architecture
+    {
+        /// <summary>
+        /// 32 bits.
+        /// </summary>
+        x86,
+        /// <summary>
+        /// 64 bits.
+        /// </summary>
+        x64,
+        /// <summary>
+        /// Using current computer's architecture.
+        /// </summary>
+        Undefined
+    }
+}
---
+++ RegUtils/ProgramScope.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegUtils
+{
+    public enum ProgramScope
+    {
+        /// <summary>
+        /// Just for one user.
+        /// </summary>
+        User,
+        /// <summary>
+        /// For everyone in this computer.
+        /// </summary>
+        Machine
+    }
+}
